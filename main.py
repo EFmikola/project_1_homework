885a3fd1e1975404376218e1e0da586aef0eeed9
Николай Ефимов <kolyamix99@gmail.com>
2025-02-16 16:10:13 +0300

    Добавлена функция sort_by_date()

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
-from src import masks as masks
-import src.widget as w
 import src.processing as proc
+import src.widget as w
+from src import masks as masks
 
 print(masks.get_mask_card_number(1234567890123456))
 print(masks.get_mask_account(73654108430135874305))
@@ -13,3 +13,4 @@ sorting_array = [
     {"id": 615064591, "state": "CANCELED", "date": "2018-10-14T08:21:33.419441"},
 ]
 print(proc.filter_by_state(sorting_array))
+print(proc.sort_by_date(sorting_array))
