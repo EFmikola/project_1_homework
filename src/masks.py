fff6ee40784daaddb3a3fb8b47b724357934d60c
Николай Ефимов <kolyamix99@gmail.com>
2025-03-04 20:50:24 +0300

    Добавлены тесты модулей masks, processing, widget

--- src/masks.py
+++ src/masks.py
@@ -1,6 +1,8 @@
 def get_mask_card_number(card_number: int) -> str:
     """Функция маскировки номера банковской карты"""
     card_number_str = str(card_number)
+    if len(card_number_str) < 16:
+        card_number_str = '0'*(16-len(card_number_str))+card_number_str
     mask_card_number = card_number_str[:4] + " " + card_number_str[4:6] + "** **** " + card_number_str[-4:]
     return mask_card_number
 
@@ -8,5 +10,7 @@ def get_mask_card_number(card_number: int) -> str:
 def get_mask_account(account_number: int) -> str:
     """Функция маскировки номера банковского счета"""
     account_number_str = str(account_number)
+    if len(account_number_str) < 20:
+        account_number_str = '0'*(20-len(account_number_str))+account_number_str
     account_number_mask = "**" + account_number_str[-4:]
     return account_number_mask
